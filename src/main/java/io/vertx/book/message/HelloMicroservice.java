a3d807d8381ac672883497a576a23cfef108ab64
Neider Tapia <neidertapia@eharmony.com>
2019-09-03 13:24:09 -0500

    add failures

--- src/main/java/io/vertx/book/message/HelloMicroservice.java
+++ src/main/java/io/vertx/book/message/HelloMicroservice.java
@@ -8,14 +8,25 @@ public class HelloMicroservice extends AbstractVerticle {
     @Override
     public void start() {
 
-        // Receive message from the address 'hello'
         vertx.eventBus().<String>consumer("hello", message -> {
-            JsonObject json = new JsonObject().put("served-by", this.toString());
-            // Check whether we have received a payload in the // incoming message
-            if (message.body().isEmpty()) {
-                message.reply(json.put("message", "hello"));
+            double chaos = Math.random();
+            JsonObject json = new JsonObject()
+                    .put("served-by", this.toString());
+            if (chaos < 0.6) {
+            // Normal behavior
+                if (message.body().isEmpty()) {
+                    message.reply(json.put("message", "hello"));
+                } else {
+                    message.reply(json.put("message", "hello "
+                            + message.body()));
+                }
+            } else if (chaos < 0.9) {
+                System.out.println("Returning a failure"); // Reply with a failure
+                message.fail(500,
+                        "message processing failure");
             } else {
-                message.reply(json.put("message", "hello " + message.body()));
+                System.out.println("Not replying");
+                // Just do not reply, leading to a timeout on the // consumer side.
             }
         });
     }
